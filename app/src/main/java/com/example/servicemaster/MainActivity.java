88d7269ee3f457a14beba2df7bb0f2f3f0aa3d33
habilithm97 <habinkim97@gmail.com>
2022-01-20 23:08:12 +0900

    Initial commit

---
+++ app/src/main/java/com/example/servicemaster/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.servicemaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*
+*서비스 : 
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
